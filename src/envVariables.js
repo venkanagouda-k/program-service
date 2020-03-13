3ecebe404f05de54ab111471a61c4b83c2cf2df6
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-03-13 10:56:59 +0530

    revert port and host for db server

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,11 +1,11 @@
 
 const envVariables = {
     baseURL: process.env.sunbird_program_base_url || 'https://dev.sunbirded.org',
-    port: process.env.sunbird_program_port || 6000,
+    port: process.env.sunbird_program_port || 5000,
     level: process.env.sunbird_service_log_level || 'info',
     config: {
         user: process.env.sunbird_program_db_user || "postgres",
-        host: process.env.sunbird_program_db_host || "172.16.0.242",
+        host: process.env.sunbird_program_db_host || "localhost",
         database: process.env.sunbird_program_db_name || 'sunbird_programs',
         password: process.env.sunbird_program_db_password || 'password',
         port: process.env.sunbird_program_db_port || 5432,
