cc7fe569324a3b205e93a73923e026d002bf2514
Vaishali Khandagale <vaishali_k@techjoomla.com>
2020-04-24 17:25:24 +0530

    Push OrgData into the nomination list if nomisation is done by orgnisation

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,6 +1,6 @@
 
 const envVariables = {
-    baseURL: process.env.dock_base_url || 'http://dock.sunbirded.org',
+    baseURL: process.env.dock_base_url || 'https://dock.sunbirded.org',
     SUNBIRD_URL: process.env.sunbird_base_url || 'https://dev.sunbirded.org',
     SUNBIRD_PORTAL_API_AUTH_TOKEN: process.env.sunbird_api_auth_token,
     DOCK_CHANNEL: process.env.dock_channel || 'sunbird',
