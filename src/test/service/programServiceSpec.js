1f121c0dcde88a028a7d68577d8ae69b3803c345
venkanagouda <shashi.kallanagoudar@gmail.com>
2020-06-29 09:49:38 +0530

    Issue #0000 feat: test cases for program-service CRUD operations

--- src/test/service/programServiceSpec.js
+++ src/test/service/programServiceSpec.js
@@ -9,12 +9,14 @@ chai.use(chaiHttp)
 const { expect } = chai
 
 const programData = require('../testData/program.json')
+const dummyData = require('../testData/dummyData');
 
 // const host = 'http://localhost:5000'
 const BASE_URL = '/program/v1'
 
 // eslint-disable-next-line no-undef
 describe('Program Service', () => {
+  let programId;
   // eslint-disable-next-line no-undef
   it('it should GET all programs', (done) => {
     chai.request(app)
@@ -41,6 +43,65 @@ describe('Program Service', () => {
       .post(BASE_URL + '/create')
       .set('Accept', 'application/json')
       .send(program)
+      // eslint-disable-next-line handle-callback-err
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
+        done()
+      })
+  })
+
+  // eslint-disable-next-line no-undef
+  it('it should get program', (done) => {
+    chai.request(app)
+      .get(BASE_URL + '/read/' + programId)
+      // eslint-disable-next-line handle-callback-err
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
+        done()
+      })
+  })
+
+  // eslint-disable-next-line no-undef
+  it('it should update a program', (done) => {
+    const programUpdate = { request: dummyData.programUpdate }
+    programUpdate.request.program_id = programId;
+    chai.request(app)
+      .post(BASE_URL + '/update')
+      .set('Accept', 'application/json')
+      .send(programUpdate)
+      // eslint-disable-next-line handle-callback-err
+      .end((err, res) => {
+        if (!programId) {
+          expect(res.status).to.equal(400)
+        } else {
+          expect(res.status).to.equal(200)
+        }
+        done()
+      })
+  })
+
+  // eslint-disable-next-line no-undef
+  it('it should add a nomination', (done) => {
+    const nominationAdd = { request: dummyData.nominationAdd }
+    nominationAdd.request.program_id = programId
+    chai.request(app)
+      .post(BASE_URL + '/nomination/add')
+      .set('Accept', 'application/json')
+      .send(nominationAdd)
+      // eslint-disable-next-line handle-callback-err
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
+        done()
+      })
+  })
+
+  // eslint-disable-next-line no-undef
+  it('it should list nominations', (done) => {
+    const nominationList = { request: {filters: dummyData.nominationList} }
+    chai.request(app)
+      .post(BASE_URL + '/nomination/list')
+      .set('Accept', 'application/json')
+      .send(nominationList)
       // eslint-disable-next-line handle-callback-err
       .end((err, res) => {
         expect(res.status).to.equal(200)
